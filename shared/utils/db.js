5cce7bd2081effd2beca49354b1dd00442c27da2
ridenaio <r@idena.io>
2021-01-13 01:25:55 +0500

    Increase db version

--- shared/utils/db.js
+++ shared/utils/db.js
@@ -1,7 +1,6 @@
 import Dexie from 'dexie'
 
 const db = new Dexie('IdenaStore')
-db.version(1).stores({flips: '&hash,epoch'})
-db.version(1).stores({ownFlips: '&id,epoch'})
+db.version(2).stores({flips: '&hash,epoch', ownFlips: '&id,epoch'})
 
 export default db
