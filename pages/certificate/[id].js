efbe9f950cc807a92378ae28dc2fa92c601e3261
ridenaio <r@idena.io>
2021-10-24 14:37:02 +0500

    Update url again

--- pages/certificate/[id].js
+++ pages/certificate/[id].js
@@ -124,9 +124,11 @@ export default function Certificate({id, certificate}) {
               target="_blank"
               href={`https://telegram.me/share/url?url=${encodeURIComponent(
                 `https://app.idena.io/certificate/${id}`
-              )}&text=I+am+looking+for+an+invitation+code+to+join+Idena.+Please+find+my+${CertificateTypeTitle(
-                certificate.type
-              ).toUpperCase()}+training+validation+certificate+in+the+link+attached.`}
+              )}&text=${encodeURIComponent(
+                `I am looking for an invitation code to join Idena. Please find my ${CertificateTypeTitle(
+                  certificate.type
+                ).toUpperCase()} training validation certificate in the link attached.`
+              )}`}
               _hover={{color: 'blue.500'}}
             >
               <TelegramIcon boxSize={4} />
