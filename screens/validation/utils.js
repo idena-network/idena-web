40973f4fdc58db638bd65c990641f64c46a3f0d3
ridenaio <r@idena.io>
2021-11-09 12:19:11 +0500

    Determine mobile on validation

--- screens/validation/utils.js
+++ screens/validation/utils.js
@@ -1,5 +1,6 @@
 import {State} from 'xstate'
 import dayjs, {isDayjs} from 'dayjs'
+import {isMobile} from 'react-device-detect'
 import {loadPersistentStateValue} from '../../shared/utils/persist'
 import {EpochPeriod, IdentityStatus} from '../../shared/types'
 import db from '../../shared/utils/db'
@@ -246,6 +247,10 @@ export function availableReportsNumber(flips) {
   return Math.floor(flips.length / 3)
 }
 
-export const webClientType = parseInt(
-  process.env.NEXT_PUBLIC_WEB_CLIENT_TYPE || 3
+const browserClientType = parseInt(process.env.NEXT_PUBLIC_WEB_CLIENT_TYPE || 3)
+
+const mobileClientType = parseInt(
+  process.env.NEXT_PUBLIC_WEB_CLIENT_TYPE_MOBILE || 4
 )
+
+export const webClientType = isMobile ? mobileClientType : browserClientType
