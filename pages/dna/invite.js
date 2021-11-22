8eb2e91d4e149b161ea3549dcbe698ba98fc1a22
aidenaio <a@idena.io>
2021-11-23 02:02:07 +0500

    Redirect dna invite only when authenticated

--- pages/dna/invite.js
+++ pages/dna/invite.js
@@ -2,16 +2,23 @@ import * as React from 'react'
 import {Spinner} from '@chakra-ui/react'
 import Layout from '../../shared/components/layout'
 import {DnaLinkMethod, useDnaLinkRedirect} from '../../screens/dna/hooks'
+import {useAuthState} from '../../shared/providers/auth-context'
 
 export default function DnaInvitePage() {
-  useDnaLinkRedirect(
-    DnaLinkMethod.Invite,
-    ({address}) => `/contacts?new&address=${address}`
-  )
+  useDnaInviteRedirect()
 
   return (
     <Layout canRedirect={false}>
       <Spinner />
     </Layout>
+  )
+}
+
+function useDnaInviteRedirect() {
+  const {auth} = useAuthState()
+
+  useDnaLinkRedirect(
+    auth && DnaLinkMethod.Invite,
+    ({address}) => `/contacts?new&address=${address}`
   )
 }
