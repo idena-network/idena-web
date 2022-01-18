e22fdf33e457913f8562dac57cfe4d3d663812aa
ridenaio <r@idena.io>
2022-01-18 14:29:57 +0500

    Change onboarding text

--- screens/home/components.js
+++ screens/home/components.js
@@ -585,39 +585,16 @@ export const ActivateInviteOnboardingContent = ({onDismiss}) => {
   return (
     <OnboardingPopoverContent
       gutter={10}
-      title={t('How to get an invitation code')}
+      title={t('Activate invitation')}
       zIndex={2}
       onDismiss={onDismiss}
     >
       <Stack>
         <Text fontSize="sm">
           {t(
-            'Join the official Idena public Telegram group and follow instructions in the pinned message.'
+            'Find an invitation code and activate it as soon as possible. Invitation code expires in 5 minutes before validation.'
           )}
         </Text>
-        <OnboardingPopoverContentIconRow
-          icon={<TelegramIcon boxSize={6} />}
-          _hover={{
-            bg: '#689aff',
-          }}
-          px={4}
-          py={2}
-          cursor="pointer"
-          onClick={() => {
-            const win = openExternalUrl('https://t.me/IdenaNetworkPublic')
-            win.focus()
-          }}
-          borderRadius="lg"
-        >
-          <Box>
-            <Text p={0} py={0} h={18} fontSize="md">
-              https://t.me/IdenaNetworkPublic
-            </Text>
-            <Text fontSize="sm" color="rgba(255, 255, 255, 0.56)">
-              {t('Official group')}
-            </Text>
-          </Box>
-        </OnboardingPopoverContentIconRow>
       </Stack>
     </OnboardingPopoverContent>
   )
