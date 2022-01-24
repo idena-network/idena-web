98078fb599a41bc4e76d43f894ebbca881f3ffd6
ridenaio <r@idena.io>
2022-01-24 19:47:56 +0500

    Change DB version

--- shared/utils/db.js
+++ shared/utils/db.js
@@ -2,7 +2,7 @@ import Dexie from 'dexie'
 
 const db = new Dexie('IdenaStore')
 
-db.version(6).stores({
+db.version(5).stores({
   flips: '&hash,epoch',
   ownFlips: '&id,epoch',
   invites: '&id,address,firstName,lastName',
