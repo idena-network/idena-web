43427f5780fcec845a9e57a298e1a49217d8ef1d
ridenaio <50695987+ridenaio@users.noreply.github.com>
2022-02-01 00:17:53 +0500

    Fix rent page \ remove table.js (#207)

--- shared/components/index.js
+++ shared/components/index.js
@@ -2,7 +2,6 @@
 export {default as Box} from './box'
 export * from './typo'
 export * from './tooltip'
-export * from './table'
 export {default as Drawer} from './drawer'
 export * from './position'
 export {default as Link} from './link'
