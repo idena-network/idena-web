f11c83d0cdda13afa866a6dd4b0bc5d89a964ea2
ridenaio <50695987+ridenaio@users.noreply.github.com>
2022-06-09 13:23:55 +0500

    Update deps, remove unused files (#266)

--- shared/utils/node.js
+++
@@ -1,9 +0,0 @@
-import {diff, valid} from 'semver'
-
-export function isHardFork(currentVersion, targetVersion) {
-  if (valid(currentVersion) && valid(targetVersion)) {
-    return ['minor', 'major'].includes(diff(currentVersion, targetVersion))
-  }
-
-  return false
-}
