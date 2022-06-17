1d081d57937809924b7bf0675a9b9b313bf28fe7
aidenaio <50833934+aidenaio@users.noreply.github.com>
2022-06-17 20:00:31 +0500

    Allow float amount in dna input (#277)
    * Allow float amount in dna input
    
    * Call onChange conditionally

--- screens/oracles/components.js
+++ screens/oracles/components.js
@@ -187,7 +187,7 @@ export function DnaInput(props) {
 
   return (
     <ChainedInputGroup>
-      <ChainedNumberInput min={0} {...props} />
+      <ChainedNumberInput min={0} step="any" {...props} />
       <ChainedInputAddon isDisabled={isDisabled}>iDNA</ChainedInputAddon>
     </ChainedInputGroup>
   )
@@ -248,7 +248,8 @@ export function NumberInput({
         const {id, value} = target
         if (!target.checkValidity()) {
           const clampedValue = clampValue(min, max, Number(value))
-          onChange({
+          // eslint-disable-next-line no-unused-expressions
+          onChange?.({
             target: {
               id,
               value: clampedValue,
