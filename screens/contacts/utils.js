f18e15a027cbfd6c2c42be09a953ac102bd5f4ad
ridenaio <50695987+ridenaio@users.noreply.github.com>
2022-08-29 13:29:34 +0500

    Do not kill Newbie invitee (#315)

--- screens/contacts/utils.js
+++ screens/contacts/utils.js
@@ -2,5 +2,4 @@ import {IdentityStatus} from '../../shared/types'
 
 export const canKill = (invitee, identity) =>
   identity?.state === IdentityStatus.Invite ||
-  (Boolean(invitee) &&
-    [IdentityStatus.Newbie, IdentityStatus.Candidate].includes(identity?.state))
+  (Boolean(invitee) && identity?.state === IdentityStatus.Candidate)
