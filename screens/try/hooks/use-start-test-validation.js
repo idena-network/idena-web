c84a5ca31c752c6a747143e951a743d8b10e4bba
aidenaio <50833934+aidenaio@users.noreply.github.com>
2022-11-08 13:35:46 +0500

    Add validation ad animation (#349)

--- screens/try/hooks/use-start-test-validation.js
+++ screens/try/hooks/use-start-test-validation.js
@@ -1,6 +1,7 @@
 import {useRouter} from 'next/router'
 import React from 'react'
 import {useInterval} from '../../../shared/hooks/use-interval'
+import {useAuthState} from '../../../shared/providers/auth-context'
 import {useTestValidationState} from '../../../shared/providers/test-validation-context'
 import {EpochPeriod} from '../../../shared/types'
 
@@ -28,12 +29,26 @@ export function useStartTestValidation() {
 export function useAutoStartTestLottery() {
   const router = useRouter()
 
+  const {coinbase} = useAuthState()
+
   const {current} = useTestValidationState()
   const period = current?.period
 
   useInterval(() => {
     if (period === EpochPeriod.FlipLottery) {
-      router.push('/try/lottery')
+      try {
+        const didCloseTestLotteryScreen = JSON.parse(
+          sessionStorage.getItem('didCloseTestLotteryScreen')
+        )
+
+        const isSameIdentity = didCloseTestLotteryScreen?.address === coinbase
+
+        if (!isSameIdentity) router.push('/try/lottery')
+      } catch (e) {
+        console.error(e)
+
+        router.push('/try/lottery')
+      }
     }
   }, 1000)
 }
